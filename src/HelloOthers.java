f178e9df002027e33b82745fb4ac9e65935f70c4
szt2palpat <szt2palpat@vasvari.org>
2023-09-19 12:55:33 +0200

    dun

---
+++ src/HelloOthers.java
@@ -0,0 +1,8 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Tamás!");
+        System.out.println("Hello Patrik!");
+        System.out.println("Hello Máté!");
+
+    }
+}
